1a31bdff94a2b205ff8c008e9652dfbf2c8fc4e4
Horacio <lopeznicora1@gmail.com>
2024-02-02 14:13:35 -0500

    Update ggplot tracking.

--- GGplot2-2.R
+++ GGplot2-2.R
@@ -6,7 +6,7 @@
 library(readxl)
 library(tidyverse)
 Farm <- read_excel("Farm_system.xlsx")
-child_data <- read_excel("Child_data.xlsx")
+#child_data <- read_excel("Child_data.xlsx")
 
 #Plot bacillulus vs acidimicrobium
 ggplot(Farm, aes(x=acidimicrobium, y=bacillus)) + 
